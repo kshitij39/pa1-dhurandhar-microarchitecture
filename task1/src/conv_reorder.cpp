9daacf59bb538eaae835b6bf8d0ab108a530f164
bitlingusaikumar <bitlingusaikumar4@gmail.com>
2026-09-02 21:15:26 +0000

    Optimize loop indexing in reorder convolution

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -1,32 +1,37 @@
-// conv_reorder.cpp  STAGE 1: LOOP REORDERING
-// Hint: loops from outermost to innermost -> ky, kx, oy, ox.
-
 #include "convolution.h"
 
-void conv_reorder(const float* in, float* out, const float* ker,
-                  int H, int W, int K) {
-
+void conv_reorder(const float* in, float* out, const float* ker,int H, int W, int K) {
 
     const int p = K / 2;
     const int in_stride = W + 2 * p;
 
-
+    
     for (int oy = 0; oy < H; ++oy) {
         for (int ox = 0; ox < W; ++ox) {
             out[oy * W + ox] = 0.0f;
         }
     }
 
+    
     for (int ky = 0; ky < K; ++ky) {
+
+        const int input_row_offset = ky * in_stride;
+        const int kernel_row_offset = ky * K;
+
         for (int kx = 0; kx < K; ++kx) {
 
-            const float weight = ker[ky * K + kx];
+            const float weight = ker[kernel_row_offset + kx];
+            const int input_col_offset = kx;
 
             for (int oy = 0; oy < H; ++oy) {
+
+                const int out_row = oy * W;
+                const int in_row = (oy * in_stride)+ input_row_offset + input_col_offset;
+
                 for (int ox = 0; ox < W; ++ox) {
 
-                    out[oy * W + ox] +=
-                        in[(oy + ky) * in_stride + (ox + kx)] * weight;
+                    out[out_row + ox] +=
+                        in[in_row + ox] * weight;
                 }
             }
         }
