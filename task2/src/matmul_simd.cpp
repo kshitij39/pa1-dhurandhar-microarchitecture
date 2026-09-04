9fbeb0fcb66e8a2e85df82f18c9372792b57827d
Tejas Itankar <tejasitankar10@gmail.com>
2026-09-04 16:15:16 +0530

    chaliye shuru krte haiiiiiiiii

--- task2/src/matmul_simd.cpp
+++ task2/src/matmul_simd.cpp
@@ -6,5 +6,7 @@
 void matmul_simd(const float* A, const float* B, float* C,
                  int M, int N, int K, int lda, int ldb, int ldc) {
     // TODO(student): replace this placeholder with your register-tiled AVX2 implementation.
+
+    
     matmul_naive(A, B, C, M, N, K, lda, ldb, ldc);
 }
