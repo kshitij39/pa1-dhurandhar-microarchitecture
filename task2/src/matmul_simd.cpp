971a5f68e31ba99a37a115084525a50f4b950815
Tejas Itankar <tejasitankar10@gmail.com>
2026-09-05 19:57:35 +0530

    made loop unrolling at i . earlier was 1*4 (a*b) now 4*4 (a*b) 16 values of c in 1 loop of i. speedup: 17x-19x. tried 2*4 got 13.24x-15.9x. tried 5*5,6*4 but wasnt improving from 4*4 .

--- task2/src/matmul_simd.cpp
+++ task2/src/matmul_simd.cpp
@@ -3,34 +3,85 @@
 
 #include "matmul.h"
 
+
+float reduce_sum(__m256 sum){
+    // sum values are reduced as first 4 in low and last 4 in high. then vertical sum of low and high
+    // ex: sum0 = [1,2,3,4,5,6,7,8] -> low[1,2,3,4] high->[5,6,7,8]
+    // now vertical sum of high and low
+    // [1+5, 2+6, 3+7, 4+8]
+    // now horizontal addition
+    // [(1+5) + (2+6), (3+7) + (4+8)]
+    // now again horizontal sum which gives one last final value
+
+    // get first 4 values
+    __m128 low = _mm256_castps256_ps128(sum);
+
+    // get last 4 values
+    __m128 high = _mm256_extractf128_ps(sum,1);
+
+    // vertical sum
+    low = _mm_add_ps(low,high);
+    // horizontal sum
+    low = _mm_add_ps(low,low);
+    // again horizontal sum
+    low = _mm_hadd_ps(low,low);
+    
+    // extracting final value from low
+    return _mm_cvtss_f32(low);
+
+}
+
 void matmul_simd(const float* A, const float* B, float* C,
                  int M, int N, int K, int lda, int ldb, int ldc) {
     // TODO(student): replace this placeholder with your register-tiled AVX2 implementation.
 
-    for(int i=0;i<M;i++){
+    for(int i=0;i+3<M;i+=4){
         
-        // one row of a
-        // processing 4 columns of b at a time
-        // c will be like:
+        // four row of a
+        // 4 columns of b 
+        // c will be like a block of 4*4
+        // c[i][j]   c[i][j+1]   c[i][j+2]   c[i][j+3]
+        // c[i+1][j] c[i+1][j+1] c[i+1][j+2] c[i+1][j+3]
+        // c[i+2][j] c[i+2][j+1] c[i+2][j+2] c[i+2][j+3]
+        // c[i+3][j] c[i+3][j+1] c[i+3][j+2] c[i+3][j+3]
+
         // c[i][j]   = row i of a * column j of b
-        // c[i][j+1] = row i of a * column j+1 of b
-        // c[i][j+2] = row i of a * column j+2 of b
-        // c[i][j+3] = row i of a * column j+3 of b
+        
         
         for(int j=0;j<N;j+=4){
 
             // sum are temps for storing a block * b block and adding them to their previous values
             // i.e. sum0 = sum0 + one block of row i of a * one block of solumn j of b
             // sum are 8 values size and summated after k loop completes i.e after one complete column and row ends
-            __m256 sum0 = _mm256_setzero_ps();
-            __m256 sum1 = _mm256_setzero_ps();
-            __m256 sum2 = _mm256_setzero_ps();
-            __m256 sum3 = _mm256_setzero_ps();
+            __m256 sum00 = _mm256_setzero_ps();
+            __m256 sum01 = _mm256_setzero_ps();
+            __m256 sum02 = _mm256_setzero_ps();
+            __m256 sum03 = _mm256_setzero_ps();
+
+
+            __m256 sum10 = _mm256_setzero_ps();
+            __m256 sum11 = _mm256_setzero_ps();
+            __m256 sum12 = _mm256_setzero_ps();
+            __m256 sum13 = _mm256_setzero_ps();
+
+            __m256 sum20 = _mm256_setzero_ps();
+            __m256 sum21 = _mm256_setzero_ps();
+            __m256 sum22 = _mm256_setzero_ps();
+            __m256 sum23 = _mm256_setzero_ps();
+
+            __m256 sum30 = _mm256_setzero_ps();
+            __m256 sum31 = _mm256_setzero_ps();
+            __m256 sum32 = _mm256_setzero_ps();
+            __m256 sum33 = _mm256_setzero_ps();
+
         
             for(int k=0;k<K;k+=8){
 
                 // loading 8 elements from row i of a
-                __m256 a = _mm256_loadu_ps(&A[i*lda + k]);
+                __m256 a0 = _mm256_loadu_ps(&A[i*lda + k]);
+                __m256 a1 = _mm256_loadu_ps(&A[(i+1)*lda + k]);
+                __m256 a2 = _mm256_loadu_ps(&A[(i+2)*lda + k]);
+                __m256 a3 = _mm256_loadu_ps(&A[(i+3)*lda + k]);
 
                 // loading 8 elements from columns of b
                 // b0 corresponds to column j 
@@ -47,10 +98,25 @@ void matmul_simd(const float* A, const float* B, float* C,
                 //sum = sum + a*b
                 //ex; sum0 = sum0 + a*b0
                 
-                sum0 = _mm256_fmadd_ps(a,b0,sum0);
-                sum1 = _mm256_fmadd_ps(a,b1,sum1);
-                sum2 = _mm256_fmadd_ps(a,b2,sum2);
-                sum3 = _mm256_fmadd_ps(a,b3,sum3);
+                sum00 = _mm256_fmadd_ps(a0,b0,sum00);
+                sum01 = _mm256_fmadd_ps(a0,b1,sum01);
+                sum02 = _mm256_fmadd_ps(a0,b2,sum02);
+                sum03 = _mm256_fmadd_ps(a0,b3,sum03);
+
+                sum10 = _mm256_fmadd_ps(a1,b0,sum10);
+                sum11 = _mm256_fmadd_ps(a1,b1,sum11);
+                sum12 = _mm256_fmadd_ps(a1,b2,sum12);
+                sum13 = _mm256_fmadd_ps(a1,b3,sum13);
+
+                sum20 = _mm256_fmadd_ps(a2,b0,sum20);
+                sum21 = _mm256_fmadd_ps(a2,b1,sum21);
+                sum22 = _mm256_fmadd_ps(a2,b2,sum22);
+                sum23 = _mm256_fmadd_ps(a2,b3,sum23);
+
+                sum30 = _mm256_fmadd_ps(a3,b0,sum30);
+                sum31 = _mm256_fmadd_ps(a3,b1,sum31);
+                sum32 = _mm256_fmadd_ps(a3,b2,sum32);
+                sum33 = _mm256_fmadd_ps(a3,b3,sum33);
 
             }
 
@@ -69,43 +135,29 @@ void matmul_simd(const float* A, const float* B, float* C,
             // now horizontal addition
             // [(1+5) + (2+6), (3+7) + (4+8)]
             // now again horizontal sum which gives one last final value
-
-            // first 4 of sum0
-            __m128 sum0_low = _mm256_castps256_ps128(sum0);
-            // last 4 of sum0
-            __m128 sum0_high = _mm256_extractf128_ps(sum0,1);
-
-            //verical sum
-            sum0_low = _mm_add_ps(sum0_low,sum0_high);
-            //horizontal sum
-            sum0_low = _mm_hadd_ps(sum0_low, sum0_low);
-            //again horizontal sum
-            sum0_low = _mm_hadd_ps(sum0_low, sum0_low);
-            //now one value in sum0_ low that is c[i][j]
-            // same apply for sum1,2,3
-
-            __m128 sum1_low = _mm256_castps256_ps128(sum1);
-            __m128 sum1_high = _mm256_extractf128_ps(sum1,1);
-            sum1_low = _mm_add_ps(sum1_low,sum1_high);
-            sum1_low = _mm_hadd_ps(sum1_low, sum1_low);
-            sum1_low = _mm_hadd_ps(sum1_low, sum1_low);
-
-            __m128 sum2_low = _mm256_castps256_ps128(sum2);
-            __m128 sum2_high = _mm256_extractf128_ps(sum2,1);
-            sum2_low = _mm_add_ps(sum2_low,sum2_high);
-            sum2_low = _mm_hadd_ps(sum2_low, sum2_low);
-            sum2_low = _mm_hadd_ps(sum2_low, sum2_low);   
+            // for this using function reduce sum
             
-            __m128 sum3_low = _mm256_castps256_ps128(sum3);
-            __m128 sum3_high = _mm256_extractf128_ps(sum3,1);
-            sum3_low = _mm_add_ps(sum3_low,sum3_high);
-            sum3_low = _mm_hadd_ps(sum3_low, sum3_low);
-            sum3_low = _mm_hadd_ps(sum3_low, sum3_low);
 
-            C[i*ldc + j]   = _mm_cvtss_f32(sum0_low);
-            C[i*ldc + j+1] = _mm_cvtss_f32(sum1_low);
-            C[i*ldc + j+2] = _mm_cvtss_f32(sum2_low);
-            C[i*ldc + j+3] = _mm_cvtss_f32(sum3_low);
+            C[i*ldc + j]   = reduce_sum(sum00);
+            C[i*ldc + j+1] = reduce_sum(sum01);
+            C[i*ldc + j+2] = reduce_sum(sum02);
+            C[i*ldc + j+3] = reduce_sum(sum03);
+
+            C[(i+1)*ldc + j]   = reduce_sum(sum10);
+            C[(i+1)*ldc + j+1] = reduce_sum(sum11);
+            C[(i+1)*ldc + j+2] = reduce_sum(sum12);
+            C[(i+1)*ldc + j+3] = reduce_sum(sum13);
+
+            C[(i+2)*ldc + j]   = reduce_sum(sum20);
+            C[(i+2)*ldc + j+1] = reduce_sum(sum21);
+            C[(i+2)*ldc + j+2] = reduce_sum(sum22);
+            C[(i+2)*ldc + j+3] = reduce_sum(sum23);
+
+            C[(i+3)*ldc + j]   = reduce_sum(sum30);
+            C[(i+3)*ldc + j+1] = reduce_sum(sum31);
+            C[(i+3)*ldc + j+2] = reduce_sum(sum32);
+            C[(i+3)*ldc + j+3] = reduce_sum(sum33);
+
 
         }
     }
