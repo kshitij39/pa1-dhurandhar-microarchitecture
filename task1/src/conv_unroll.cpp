78287ff8ebe5f314259f7de8057649cbbf706445
bitlingusaikumar <bitlingusaikumar4@gmail.com>
2026-09-07 06:51:02 +0000

    sequence of codes lines changed for acc and out.

--- task1/src/conv_unroll.cpp
+++ task1/src/conv_unroll.cpp
@@ -25,25 +25,27 @@ void conv_unroll(const float* in, float* out, const float* ker,
                 for (int kx = 0; kx < K; ++kx) {
                     const float weight = ker[ker_row + kx];
 
-                    acc0 += in[in_row + kx]     * weight;
-                    acc1 += in[in_row + kx + 1] * weight;
-                    acc2 += in[in_row + kx + 2] * weight;
-                    acc3 += in[in_row + kx + 3] * weight;
-                    acc4 += in[in_row + kx + 4] * weight;
-                    acc5 += in[in_row + kx + 5] * weight;
-                    acc6 += in[in_row + kx + 6] * weight;
+                
                     acc7 += in[in_row + kx + 7] * weight;
+                    acc2 += in[in_row + kx + 2] * weight;
+                    acc5 += in[in_row + kx + 5] * weight;
+                    acc4 += in[in_row + kx + 4] * weight;
+                    acc3 += in[in_row + kx + 3] * weight;
+                    acc6 += in[in_row + kx + 6] * weight;
+                    acc1 += in[in_row + kx + 1] * weight;
+                    acc0 += in[in_row + kx] * weight;
                 }
             }
 
-            out[oy * W + ox]     = acc0;
-            out[oy * W + ox + 1] = acc1;
-            out[oy * W + ox + 2] = acc2;
-            out[oy * W + ox + 3] = acc3;
-            out[oy * W + ox + 4] = acc4;
-            out[oy * W + ox + 5] = acc5;
-            out[oy * W + ox + 6] = acc6;
+            
             out[oy * W + ox + 7] = acc7;
+            out[oy * W + ox + 2] = acc2;
+            out[oy * W + ox + 5] = acc5;
+            out[oy * W + ox + 4] = acc4;
+            out[oy * W + ox + 3] = acc3;
+            out[oy * W + ox + 6] = acc6;
+            out[oy * W + ox + 1] = acc1;
+             out[oy * W + ox] = acc0;
         }
     }
 }
